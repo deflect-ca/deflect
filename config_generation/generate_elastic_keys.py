69bc3af8ccefe4dbfba2cf5acb6030f84b17ca85
Joe Landers <joe@joelanders.net>
2021-11-02 18:53:25 +0100

    some versions of python's cryptography require specifying the backend

--- config_generation/generate_elastic_keys.py
+++ config_generation/generate_elastic_keys.py
@@ -3,6 +3,7 @@
 from cryptography import x509
 from cryptography.hazmat.primitives import hashes, serialization
 from cryptography.hazmat.primitives.asymmetric import rsa
+from cryptography.hazmat.backends import default_backend
 from cryptography.x509.oid import NameOID
 import ipaddress
 import datetime
@@ -19,6 +20,7 @@ def gen_key_and_cert(name, alt_name, issuer_name, is_ca, logger):
     priv_key = rsa.generate_private_key(
         public_exponent=65537,
         key_size=2048,
+        backend=default_backend()
     )
 
     pub_key = priv_key.public_key()
