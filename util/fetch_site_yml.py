19257b644acc8f6cd99201e1d767d4e6431788ad
Jeremy Yen <jeremy5189@gmail.com>
2021-12-10 19:29:55 +0800

    Bug fixed

--- util/fetch_site_yml.py
+++ util/fetch_site_yml.py
@@ -55,27 +55,27 @@ def fetch_site_yml(config, logger):
     scp = SCPClient(ssh.get_transport(), progress=progress)
 
     # 1. Get clients.yml
-    dst = f"{config['scp_dst']}/clients.yml-{dashdate}"
+    dst = f"{config['config_root']}/{config['scp_dst']}/clients.yml-{dashdate}"
     logger.debug("scp {} to {}".format(config['scp_src'], dst))
     scp.get(config['scp_src'], dst)
 
     # 2. Sym link
-    ln_src = dst
-    ln_dst = f"{config['scp_dst']}/../old-sites.yml"
+    ln_src = f"{config['scp_dst']}/clients.yml-{dashdate}"
+    ln_dst = f"{config['config_root']}/old-sites.yml"
     logger.debug("ln {} to {}".format(ln_src, ln_dst))
     symlink_force(ln_src, ln_dst)
 
     # XXX: only copy the site TLS in client.yml
     # 3. TLS
     logger.debug("Get TLS bundles")
-    scp.get(config['tls_src'], config['tls_dst'], recursive=True)
+    scp.get(config['tls_src'], f"{config['config_root']}/{config['tls_dst']}", recursive=True)
 
     logger.debug("Closing SCP and SSH client")
     scp.close()
     ssh.close()
 
     # 4. Mark last_used
-    last_used = f"{config['scp_dst']}/clients.yml-last_used"
+    last_used = f"{config['config_root']}/{config['scp_dst']}/clients.yml-last_used"
     if not path.exists(last_used):
         logger.debug("cp dst to last_used".format(dst, last_used))
         copyfile(dst, last_used)
