bf764fefd30d91ea281e39fd5563e8d5fd2d5785
Miguel <miguel@equalit.ie>
2022-01-25 20:43:12 -0600

    Add a Container class for EdgeManage

---
+++ orchestration/run_container/edgemanage.py
@@ -0,0 +1,20 @@
+from orchestration.run_container.base_class import Container
+
+
+class EdgeManage(Container):
+    def update(self, config_timestamp):
+        #with open(f"output/{config_timestamp}/etc-edgemanage.tar", "rb") as f:
+        #    self.container.put_archive("/etc/edgemanage", f.read())
+        self.container.kill(signal="SIGHUP")
+
+    def start_new_container(self, config, image_id):
+        return self.client.containers.run(
+            image_id,
+            detach=True,
+            ports={},
+            labels={
+                "name": "edgemanage",
+            },
+            name="edgemanage",
+            restart_policy=Container.DEFAULT_RESTART_POLICY,
+        )
