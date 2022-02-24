3eb699e4ec86f02bf882eee371b51f8a5d4198e6
Miguel <miguel@equalit.ie>
2022-02-23 23:24:03 -0600

    Implement a mechanism to load config files with default values

---
+++ util/config_parser.py
@@ -0,0 +1,25 @@
+import copy
+
+from pyaml_env import parse_config
+
+from util.helpers import get_config_file_path, get_container_default_config_path
+
+
+def parse_with_defaults(container_name, config_name):
+    config_path = get_config_file_path(config_name)
+    defaults_path = get_container_default_config_path(container_name, config_name)
+    config = parse_config(config_path)
+    defaults = parse_config(defaults_path)
+    return set_defaults(config, defaults)
+
+
+def set_defaults(config, defaults):
+    for key, element in defaults.items():
+        if type(element) == dict:
+            if key in config:
+                set_defaults(config[key], defaults[key])
+            else:
+                config[key] = copy.deepcopy(element)
+        elif key not in config:
+            config[key] = element
+    return config
--- util/helpers.py
+++ util/helpers.py
@@ -67,6 +67,16 @@ def path_to_containers():
         module_root_path(), 'containers'
     )
 
+def get_container_path(container_name):
+    return os.path.join(path_to_containers(), container_name)
+
+def get_container_default_config_path(container_name, config_file):
+    return os.path.join(get_container_path(container_name),
+                        f"default-{config_file}")
+
+def get_config_file_path(config_file):
+    return os.path.join(path_to_input(), f'config/{config_file}')
+
 def get_sites_yml_path():
     return os.path.join(path_to_input(), 'config/old_sites.yml')
 
@@ -81,9 +91,6 @@ def get_persisted_config_yml_path():
 
 def get_banjax_config_yml_path():
     return os.path.join(path_to_input(), 'config/banjax_config.yml')
-
-def get_edgemanage_yaml_path():
-    return os.path.join(path_to_input(), 'config/edgemanage.yaml')
 
 def get_kibana_saved_objects_path():
     return os.path.join(path_to_input(), 'kibana-saved-objects.ndjson')
