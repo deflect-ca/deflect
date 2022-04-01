f07036944cb5e12c30e6126bed075399c38d4609
Jeremy Yen <jeremy5189@gmail.com>
2022-04-01 16:07:46 +0800

    adjust log format

--- util/helpers.py
+++ util/helpers.py
@@ -50,7 +50,7 @@ def get_logger(name, log_level=None, output_file='deflect-next.log'):
 
         # create formatter and add it to the handlers
         formatter = logging.Formatter(
-            '[%(levelname)s] %(asctime)s %(name)s.%(funcName)s:%(lineno)s | %(message)s'
+            '[%(levelname)s] %(asctime)s %(funcName)s:%(lineno)s | %(message)s'
         )
         file_handler.setFormatter(formatter)
         console_handler.setFormatter(formatter)
