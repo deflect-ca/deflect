5db60dbe56dca76f7ee07ee58fe1d13173045c01
Jeremy Yen <jeremy5189@gmail.com>
2022-04-15 17:26:03 +0800

    Code to support banjax SIGHUP

--- orchestration/run_container/banjax.py
+++ orchestration/run_container/banjax.py
@@ -7,20 +7,8 @@ class Banjax(Container):
         with open(f"output/{config_timestamp}/etc-banjax.tar", "rb") as f:
             self.container.put_archive("/etc/banjax", f.read())
 
-        # XXX config reload not implemented yet
-        #  banjax_container.kill(signal="SIGHUP")
-        # XXX: Temp solution to restart banjax and legacy-filebeat (for volumne)
-        #      until we get banjax SIGHUP config reload fixed
-        self.logger.info('Restarting banjax container')
-        self.container.restart()
-        legacy_filebeat_container = find_existing_container(
-            self.client, "legacy-filebeat",
-            extra_label=None, config=None, logger=self.logger)
-        if legacy_filebeat_container:
-            self.logger.info('Restarting legacy-filebeat container for its volume to banjax')
-            legacy_filebeat_container.restart()
-        else:
-            self.logger.info('Could not find legacy-filebeat container to restart')
+        self.logger.info('Sending SIGHUP to banjax container')
+        self.container.kill(signal="SIGHUP")
 
     def start_new_container(self, config, image_id):
         # XXX consider a different approach (making the caller pass in the network and fs namespaces?)
