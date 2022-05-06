c64df3f14c07228c4c4b2c8babbcd4a7315f3da0
Jeremy Yen <jeremy5189@gmail.com>
2022-05-06 18:55:48 +0800

    Fix #80, tar certs before scp

--- util/fetch_site_yml.py
+++ util/fetch_site_yml.py
@@ -12,7 +12,6 @@ Ref: https://github.com/equalitie/autodeflect/blob/32bc3a3f7f3caac1b08ef94414f37
 import paramiko
 import sys
 import os
-import errno
 
 from scp import SCPClient
 from os import path
@@ -29,7 +28,7 @@ def fetch_site_yml(config, logger):
     ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
     ssh.load_system_host_keys()
 
-    logger.debug(f"SSH Connection to {config['ssh']['host']}...")
+    logger.info(f"SSH Connection to {config['ssh']['host']}...")
     ssh.connect(hostname=config['ssh']['host'],
                 username=config['ssh']['user'],
                 port=config['ssh']['port'])
@@ -39,33 +38,38 @@ def fetch_site_yml(config, logger):
     stdin, stdout, stderr = ssh.exec_command('date +%Y%m%d%H%M%S')
     lines = stdout.readlines()
     dashdate = lines[0].replace('\n', '')
-    logger.debug(f"dashdate: {dashdate}")
+    logger.info(f"dashdate: {dashdate}")
 
     logger.debug("Starting SCP client...")
     scp = SCPClient(ssh.get_transport(), progress=progress)
 
     # 1. Get clients.yml
     dst = f"{config['config_root']}/{config['scp_dst']}/clients.yml-{dashdate}"
-    logger.debug("scp {} to {}".format(config['scp_src'], dst))
+    logger.info("scp {} to {}".format(config['scp_src'], dst))
     scp.get(config['scp_src'], dst)
 
     # 2. Sym link
     ln_src = f"{config['scp_dst']}/clients.yml-{dashdate}"
     ln_dst = f"{config['config_root']}/old_sites.yml"
-    logger.debug("ln {} to {}".format(ln_src, ln_dst))
+    logger.info("ln {} to {}".format(ln_src, ln_dst))
     symlink_force(ln_src, ln_dst)
 
     # XXX: only copy the site TLS in client.yml
     # 3. TLS
-    logger.debug("Get TLS bundles")
-    scp.get(config['tls_src'], f"{config['config_root']}", recursive=True)
+    logger.info("Packing TLS bundles on remote")
+    ssh.exec_command(f"cd {config['tls_src']} && tar -zcf /tmp/tls_bundles.tar.gz -C {config['tls_src']} .")
+    logger.info("Get TLS bundles")
+    scp.get("/tmp/tls_bundles.tar.gz", f"{config['config_root']}")
+    logger.info("Untar TLS bundles")
+    os.system(f"mkdir -p {config['config_root']}/{config['tls_dst']}")
+    os.system(f"tar -zxf {config['config_root']}/tls_bundles.tar.gz -C {config['config_root']}/{config['tls_dst']}")
 
-    logger.debug("Closing SCP and SSH client")
+    logger.info("Closing SCP and SSH client")
     scp.close()
     ssh.close()
 
     # 4. Mark last_used
     last_used = f"{config['config_root']}/{config['scp_dst']}/clients.yml-last_used"
     if not path.exists(last_used):
-        logger.debug("cp dst to last_used".format(dst, last_used))
+        logger.info("cp dst to last_used".format(dst, last_used))
         copyfile(dst, last_used)
