fdf21a76d1501e89f67ac3f0dffade093ca1afbc
Jeremy Yen <jeremy5189@gmail.com>
2022-11-07 23:11:46 +0800

    test output json to test-origin static

--- orchestration/run_container/test_origin.py
+++ orchestration/run_container/test_origin.py
@@ -3,7 +3,9 @@ from orchestration.run_container.base_class import Container
 
 class TestOrigin(Container):
     def update(self, config_timestamp):
-        pass
+        self.container.exec_run("rm -f /opt/test-origin/static/*.json")
+        with open(f"output/{config_timestamp}/test-origin.tar", "rb") as f:
+            self.container.put_archive("/opt/test-origin/static", f.read())
 
     def start_new_container(self, config, image_id):
         return self.client.containers.run(
